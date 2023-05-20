a895b5ec8cb76ae815c00a706ac791eb76d40381
Michael Elisha <mishaelifire@gmail.com>
2023-05-20 20:15:26 +0300

    index fix

--- src/app/comp/header/header.component.ts
+++ src/app/comp/header/header.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   isOpenTrans: boolean = false;
   isOpenMenu: boolean = false;
   isOpenAcc: boolean = false;
 
+  functionScript: any;
+  jsSrvScript: any;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.functionScript = document.createElement('script');
+    this.functionScript.type = 'text/javascript';
+    this.functionScript.innerHTML = `
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement(
+          {pageLanguage: 'en'},
+          'google_translate_element'
+      );
+    }`;
+
+    this.jsSrvScript = document.createElement('script');
+    this.jsSrvScript.type = 'text/javascript';
+    this.jsSrvScript.src = `https://translate.google.com/translate_a/element.js?
+    cb=googleTranslateElementInit`;
+
+    document.body.appendChild(this.functionScript);
+    document.body.appendChild(this.jsSrvScript);
+  }
 
   openTrans(): void {
     this.isOpenTrans = !this.isOpenTrans;
