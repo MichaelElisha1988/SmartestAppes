b0c27688ec01cbaf5889046c9091ebdf1524e49b
Michael Elisha <mishaelifire@gmail.com>
2024-04-19 12:57:40 +0300

    pushing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './routes/home/home.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
