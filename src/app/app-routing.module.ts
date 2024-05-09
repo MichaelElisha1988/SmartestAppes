a21df96c8260b622862c0e3c086db8b470927802
Michael Elisha <mishaelifire@gmail.com>
2024-05-09 09:58:07 +0300

    guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,23 @@ import { HomeComponent } from './routes/home/home.component';
 import { MeasurementsComponent } from './comp/measurements/measurements.component';
 import { PolicyComponent } from './routes/policy/policy.component';
 import { ListsComponent } from './routes/lists/lists.component';
+import { LoginGuard } from './shared/guards/login.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [LoginGuard],
   },
-  { path: 'lists', component: ListsComponent },
+  { path: 'lists', component: ListsComponent, canActivate: [LoginGuard] },
   {
     path: 'policy',
     component: PolicyComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
