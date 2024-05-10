918f00b282088d2d491ba999f379f9e0fef6958c
Michael Elisha <mishaelifire@gmail.com>
2024-05-10 22:29:17 +0300

    ..

--- src/app/routes/my-recepies-book/my-recepies-book.component.ts
+++ src/app/routes/my-recepies-book/my-recepies-book.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-my-recepies-book',
   templateUrl: './my-recepies-book.component.html',
-  styleUrls: ['./my-recepies-book.component.scss']
+  styleUrls: ['./my-recepies-book.component.scss'],
 })
-export class MyRecepiesBookComponent implements OnInit {
+export class MyRecepiesBookComponent implements OnInit, OnDestroy {
+  searchForm = new FormGroup({
+    search: new FormControl('', { updateOn: 'change' }),
+  });
 
-  constructor() { }
+  Sub$ = new Subscription();
 
-  ngOnInit(): void {
+  constructor() {
+    this.Sub$.add(
+      this.searchForm.controls.search.valueChanges.subscribe((value) => {
+        console.log((value as string)?.length > 3 ? value : '');
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.Sub$.unsubscribe();
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit(): void {}
 }
